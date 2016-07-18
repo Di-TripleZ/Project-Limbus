f8c00ca866c3b978344da5dcc3a00615ab991d31
Lorenzo López Muñoz <Lorenzo Lpez Muoz>
2016-07-18 17:03:15 +0200

    Added effects and music

--- js/limbo.js
+++ js/limbo.js
@@ -17,10 +17,29 @@ app.config(['$routeProvider', function($routeProvider) {
 		.otherwise({
 			redirectTo: "/",
 		});
+
 }]);
 
 app.controller("HomeViewController", ['$scope', function($scope) {
 
+	window.addEventListener("scroll", updatePosition, false);
+
+	function updatePosition(event) {
+		topWeb = this.scrollY;
+		leftWeb = this.scrollX;
+
+
+		checkEventHome();
+		console.log(topWeb + "," + leftWeb);
+	}
+
+
+	function checkEventHome() {
+		Thunder(400);
+		showTextEffect(800, '.intertext2', 5);
+		showTextEffect(1200, '.intertext3', 1.5);
+		showFinishButton(1200);
+	}
 
 }]);
 
@@ -44,6 +63,8 @@ app.controller("WarningController", ['$scope', function($scope) {
 	$('#startBtnWarning').on('click', function() {
 		console.log('Hello World!');
 		evilLaughSound.play()
+		$('.transitionToStart').css('opacity', '1')
+		$('.transitionToStart').css('z-index', '2')
 		setTimeout(function() {
 			$(location).attr('href', '#/Darkest-Nightmares')
 		}, 5205)
@@ -53,48 +74,53 @@ app.controller("WarningController", ['$scope', function($scope) {
 
 app.controller("Ep1View1Controller", ['$scope', function($scope) {
 
+	window.addEventListener("scroll", updatePosition, false);
+
+	function updatePosition(event) {
+		topWeb = this.scrollY;
+		leftWeb = this.scrollX;
+
+
+		checkEventEp1View1();
+		console.log(topWeb + "," + leftWeb + " Ep1");
+	}
+
+	function init11() {
+		setTimeout(function() {
+			showText('.text-slide1', 4);
+		}, 1)
+		setTimeout(function() {
+			showText('.text-slide1-1', 10);
+		}, 3500);
+
+	}
+
+	init11()
+
+
+	function checkEventEp1View1() {}
+
+	tocataLoop.addEventListener('ended', function() {
+		this.currentTime = 0;
+		this.play();
+	}, false);
 
 }]);
 
 var thunderSound = new Audio('mp3/thunder.mp3');
 var evilLaughSound = new Audio('mp3/evilLaugh.ogg')
+var tocataStart = new Audio('mp3/tocata-start.wav')
+var tocataLoop = new Audio('mp3/tocata-loop.wav')
 
-var thundered = false;
 var topWeb, leftWeb;
-
-window.addEventListener("scroll", updatePosition, false);
-
-function updatePosition(event) {
-	topWeb = this.scrollY;
-	leftWeb = this.scrollX;
+var thundered = false;
+var tocataplayed = false;
 
 
-	checkEvent();
-	console.log(topWeb + "," + leftWeb);
-}
-
-
-function checkEvent() {
-	Thunder(400);
-	showTextEffect(800, '.intertext2', 5);
-	showTextEffect(1200, '.intertext3', 1.5);
-	showFinishButton(1200);
-}
 
 function Thunder(position) {
 	if (topWeb >= position - 10 && topWeb <= position + 10 && thundered === false) {
 		thunderEffect()
-	}
-}
-
-function showFinishButton(position) {
-	if (position <= topWeb) {
-		setTimeout(function() {
-			$('#bottomButtonMainPage').removeClass('hidden');
-		}, 6500);
-		setTimeout(function() {
-			$('#bottomButtonMainPage').removeClass('visuallyhidden');
-		}, 6520);
 	}
 }
 
@@ -119,4 +145,42 @@ function invertImage() {
 	$('html').toggleClass('invert');
 }
 
+function showFinishButton(position) {
+	if (position <= topWeb) {
+		setTimeout(function() {
+			$('#bottomButtonMainPage').removeClass('hidden');
+		}, 6500);
+		setTimeout(function() {
+			$('#bottomButtonMainPage').removeClass('visuallyhidden');
+		}, 6520);
+	}
+}
+
+
+function showTextEffect(position, selector, delay) {
+	if (position <= topWeb) {
+		showText(selector, delay)
+	}
+}
+
+function playTocata() {
+	if (tocataplayed === false) {
+		tocataPlayed = true
+		setTimeout(function() {
+			tocataStart.play();
+		}, 3500)
+		setTimeout(function() {
+			tocataLoop.volume = 0.1;
+			tocataLoop.play();
+		}, 16631)
+	}
+}
+
+function showText(selector, delay) {
+	$(selector).textillate({ in : { effect: 'fadeInLeftBig', initialDelay: 0, delayScale: delay } });
+	$(selector).css('display', 'inline');
+}
+
+
 $('.tlt').textillate({ in : { effect: 'fadeInLeftBig', initialDelay: 0 } });
+
--- js/parallaxRunner.js
+++ js/parallaxRunner.js
@@ -6,15 +6,3 @@
 
   	}); // end of document ready
   })(jQuery); // end of jQuery name space
-
-
-  var selectorCounter;
-  var selectorChosen;
-
-  function showTextEffect(position, selector, delay) {
-  	if (position <= topWeb) {
-  		$(selector).textillate({ in : { effect: 'fadeInLeftBig', initialDelay: 0, delayScale: delay } });
-  		$(selector).css('display', 'inline');
-  	}
-  }
-
